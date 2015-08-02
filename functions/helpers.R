5b194d6ac432e7d9e926e6fa512b0ceb8b0a03a5
Tutuchan <pierre.formont@gmail.com>
2015-08-02 18:34:00 +0200

    common function for data

--- functions/helpers.R
+++ functions/helpers.R
@@ -1,17 +1,37 @@
-getTournament <- function(id = NULL, url = mainURL){
-  url_ = paste0(url, "tournament.json")
-  if(!missing(id)) url_ <- paste0(url, "tournament/", id, ".json")
+getData <- function(type, id = NULL, url = mainURL){
+  if (!type %in% c("series", "league", "tournament")) stop('incorrect type')
+  if (type == "league") {
+    url_ = paste0(url, "league.json?parameters[method]=all&parameters[published]=1")
+    if(!is.null(id)) url_ <- paste0(url, "league/", id, ".json")
+  } else {
+    url_ = paste0(url, type, ".json")
+    if(!is.null(id)) url_ <- paste0(url, type, "/", id, ".json")
+  }
+  print(url_)
   fromJSON(url_)
+}
+
+
+getTournament <- function(id = NULL, url = mainURL){
+  getData("tournament", id, url)
 }
 
 getLeague <- function(id = NULL, url = mainURL){
-  url_ = paste0(url, "league.json?parameters[method]=all&parameters[published]=1")
-  if(!missing(id)) url_ <- paste0(url, "league/", id, ".json")
-  fromJSON(url_)
+  getData("league", id, url)
 }
 
 getSeries <- function(id = NULL, url = mainURL){
-  url_ = paste0(url, "series.json")
-  if(!missing(id)) url_ <- paste0(url, "series/", id, ".json")
-  fromJSON(url_)
+  getData("series", id, url)
+}
+
+
+getAllTeams <- function(){
+  listTournaments <- getTournament()
+  lapply(listTournaments, function(tournament){
+    if(length(tournament$contestants) > 0) unlist(tournament$contestants) %>% 
+      t %>% 
+      as.data.frame %>% 
+      select(ends_with("id")) %>% 
+      as.numeric
+  }) %>% unlist %>% as.numeric
 }
